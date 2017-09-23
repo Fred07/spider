b37f654cf689a14563988f7695227dae97e8e75f
fred07 <fivesec10@gmail.com>
2017-09-23 21:43:27 +0800

    Add new spider

--- spider_new/items.py
+++ spider_new/items.py
@@ -10,10 +10,7 @@ import scrapy
 
 class SpiderNewItem(scrapy.Item):
     # define the fields for your item here like:
-    # name = scrapy.Field()
     title = scrapy.Field()
     link = scrapy.Field()
-    post_id = scrapy.Field()
-    site = scrapy.Field()
     image_urls = scrapy.Field()
     images = scrapy.Field()
